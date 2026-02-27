c661145e0358241b7656b7ed6df66ef0fa873bac
Loligh <142368232+Loligh@users.noreply.github.com>
2026-02-27 01:27:10 +0100

    added efi mount point to configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -35,6 +35,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/efi";
 
   #networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
